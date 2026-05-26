8558e0c758c22c8e319528f2d8a8bf567f155a7e
WShashinduChamika <shashinduchamika99@gmail.com>
2026-05-26 16:53:01 +0500

    Arithmatic operators

---
+++ arithmatic_operators..py
@@ -0,0 +1,25 @@
+a = 11
+b = 3
+
+# addition
+print(a+b)
+
+# substraction
+print(a-b)
+
+# multiplication
+print(a*b)
+
+# division
+print(a/b)
+
+# module
+print(a%b)
+
+# floor division
+print(a//b)
+
+# power
+print(a**b)
+
+
