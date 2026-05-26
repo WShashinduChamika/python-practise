0daf8dc22f9ff3515b2ca7155d706fd41bbd8f81
WShashinduChamika <shashinduchamika99@gmail.com>
2026-05-26 18:24:50 +0500

    Conditionla Statements

---
+++ condition_statement.py
@@ -0,0 +1,19 @@
+marks = 70
+
+# if else 
+if marks >= 50:
+    print("Pass")
+else:
+    print("Fail")
+
+# if elseif else
+if marks >= 75:
+    print("A")
+elif marks >= 65:
+    print("B")
+elif marks >= 55:
+    print("C")
+elif marks >= 35:
+    print("S")
+else:
+    print("Fail")
