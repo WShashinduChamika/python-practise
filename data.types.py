cca9d93cf3a11f47cdce2d54de1f07d1fc448341
WShashinduChamika <shashinduchamika99@gmail.com>
2026-05-26 09:36:33 +0500

    Numeric data types

---
+++ data.types.py
@@ -0,0 +1,34 @@
+# Integer
+a = 10
+b = -25
+
+print(a)
+print(type(a))
+print(a.__sizeof__())
+
+
+# Float
+c = 11.5
+d = -2.4
+
+print(c)
+print(type(c))
+print(c.__sizeof__())
+
+# Boolean
+e = True
+f = False
+
+print(e)
+print(type(e))
+
+
+# Complex
+i = 5 +6j
+g = complex(2, 3.5)
+
+print(i)
+print(type(i))
+print(i.real)
+print(i.imag)
+print(g)
