98c3b69f4d23eb3823fe343b6eddbde85a3e8a8c
WShashinduChamika <shashinduchamika99@gmail.com>
2026-05-26 17:29:18 +0500

    Comparison Operators

---
+++ comparison_operators.py
@@ -0,0 +1,20 @@
+a = 5
+b = 9
+
+# Equality
+print(a==b)
+
+# Not Equal
+print(a!=b)
+
+# Greater Than
+print(a>b)
+
+# Greater Than or Equal
+print(a>=b)
+
+# Less Than
+print(a<b)
+
+# Less Than or Equal
+print(a<=b)
