e7eaacfe6516e3e7757e317286c98a86dd6e58c0
WShashinduChamika <shashinduchamika99@gmail.com>
2026-05-27 12:02:04 +0500

    Formatted Print Function

--- print_function.py
+++ print_function.py
@@ -3,12 +3,22 @@
 # print("Hello World \tShashindu")
 # print("Hello World \Shashindu")
 
-name = "Shashindu"
-age = 12
-score = 17.5
+# name = "Shashindu"
+# age = 12
+# score = 17.5
 
-print(name, end=" ")
-print(age)
-print(score)
+# print(name, end=" ")
+# print(age)
+# print(score)
 
-print(name, age, score)
+# print(name, age, score)
+
+name, age, score = "Kamal", 22, 43.567
+
+print("He is "+name +" . He is "+str(age)+ " years old")
+
+print("He is %s. He is %d years old, old his score is %.3f" %(name, age, score) )
+
+print("He is {}. He is {} yeard old, and his score is {}" .format(name, age, score))
+
+print(f"He is {name}")
