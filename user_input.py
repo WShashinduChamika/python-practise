0bd6bf688e21e483a74174b80c22b7bc8f8d3f61
WShashinduChamika <shashinduchamika99@gmail.com>
2026-05-27 12:16:56 +0500

    User Input

---
+++ user_input.py
@@ -0,0 +1,15 @@
+# name = input("Enter your name: ")
+# age = int(input("Enter your age: "))
+
+# print("Name is", name)
+# print(f"Age is {age}")
+
+# print(type(name))
+# print(type(age))
+
+num1 = int(input("Entter first number: "))
+num2 = int(input("Entter second number: "))
+
+sum = num1 + num2
+
+print(f"Addition of num1 and num2 is {sum}")
