b863b56b86370ebdeac10394ec45f07ccc96a2d8
WShashinduChamika <shashinduchamika99@gmail.com>
2026-05-27 13:39:43 +0500

    String Mehtod 2

---
+++ string_method2.py
@@ -0,0 +1,27 @@
+name = "Shashindu Chamika"
+
+print(name.startswith("S"))
+print(name.startswith("Sha"))
+
+print(name.endswith("S"))
+print(name.endswith("a"))
+
+
+x = "Kamal, 22, colombo"
+print(x)
+print(x.replace("Kamal","-"))
+
+x = "abc"
+y = "xyz"
+
+print(x.join(y))
+print(" ".join(y))
+
+name_arr = ["Kamal", "Saman", "Nimal"]
+print(" ".join(name_arr))
+
+name2 = "Namal Sunil Janidu"
+print(name2.split())
+
+name3 = "Sathira Nushan \n 12 34"
+print(name3.splitlines())
