c9fb856f4d665796676f431fd32ff411e6c2e424
WShashinduChamika <shashinduchamika99@gmail.com>
2026-05-27 11:49:42 +0500

    Print Functtion

---
+++ print_function.py
@@ -0,0 +1,14 @@
+# print("Hello World \nShashindu")
+# print("Hello World \rShashindu")
+# print("Hello World \tShashindu")
+# print("Hello World \Shashindu")
+
+name = "Shashindu"
+age = 12
+score = 17.5
+
+print(name, end=" ")
+print(age)
+print(score)
+
+print(name, age, score)
