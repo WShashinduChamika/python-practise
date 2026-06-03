67322b3165563331393852edf4f20605aec67c39
WShashinduChamika <shashinduchamika99@gmail.com>
2026-06-04 00:01:38 +0500

    For loop

---
+++ foor_loop.py
@@ -0,0 +1,13 @@
+x = [1,2,3,4,5]
+
+# for i in x :
+#     print(i)
+
+# my_dict = {'name':'Shashindu', 'age':20, 'city':'Kandy'}
+
+# for i,j in my_dict.items():
+#     print(i, j)
+
+for i in range(2,11,2):
+    print(i)
+
