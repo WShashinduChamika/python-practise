2c02f37d73b810a6c7ba12a051e8f4573cbff6a4
WShashinduChamika <shashinduchamika99@gmail.com>
2026-06-03 20:12:10 +0500

    List concept

---
+++ list.py
@@ -0,0 +1,45 @@
+# list1 = [20, 30,40,50]
+# list2 = list((1,2,3,4))
+
+# print(list1)
+# print(list2)
+
+# print(type(list1))
+# print(type(list2))
+
+my_list = ['kamal', 20, 'Kandy', False, 20, [1,2,3]]
+
+# print(my_list[2])
+# print(my_list[0:4])
+# print(my_list[0:8:2])
+# print(my_list[5])
+# print(my_list[5][1])
+
+# # Mutable - Can be changed during run time
+# my_list[2] = 'colombo'
+# print(my_list)
+
+# my_list.append("Shashindu") # Only one value
+# print(my_list)
+
+# my_list.extend(["Chamika", 10]) # For several values
+# print(my_list)
+
+# my_list.insert(0, 1)
+# print(my_list)
+
+# my_list.pop(0)
+# print(my_list)
+
+# my_list.remove(20)
+# print(my_list)
+
+# my_list.clear()
+# print(my_list)
+
+# my_list[:] = []
+# print(my_list)
+
+print(len(my_list))
+print(my_list.index(20))
+print(my_list.count(20))
