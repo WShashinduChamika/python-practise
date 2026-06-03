74851fe05f842845cd57f494d7b3f9aac15d6474
WShashinduChamika <shashinduchamika99@gmail.com>
2026-06-03 21:42:59 +0500

    Tupple concept

---
+++ tupple.py
@@ -0,0 +1,22 @@
+# tpl1 = (20,30,40)
+# tpl2 = tuple((1,2,3))
+
+# print(type(tpl1))
+# print(type(tpl2))
+
+# print(tpl1)
+# print(tpl2)
+
+# print(tpl1[1])
+
+# print(tpl1[0:2])
+
+# print(tpl1.count(20))
+
+# print(tpl1.index(20))
+
+list = [1,2,3,4]
+
+tpl = tuple(list)
+print(type(tpl))
+print(tpl)
