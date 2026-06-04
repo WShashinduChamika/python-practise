eacaec5b079903b80e4e64504813f2b324be263f
WShashinduChamika <shashinduchamika99@gmail.com>
2026-06-04 08:28:07 +0500

    User defined functions

---
+++ user_defined_functions.py
@@ -0,0 +1,19 @@
+def function_name(arg1,arg2):
+    print(arg1, arg2)
+
+function_name('Shashindu', 'Chamika')
+
+def sum(num1, num2):
+    sum = num1 + num2
+    print(f'Sum of the {num1} and {num2} is: {sum}')
+
+sum(1,2)
+
+def func(length,width):
+    perimeter = 2*(length + width)
+    area = length * width
+    print(perimeter)
+    print(area)
+
+func(5,3)
+func(8,2)
