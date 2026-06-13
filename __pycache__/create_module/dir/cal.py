22589954a8df54a25513384942a13fdbbe6d5707
WShashinduChamika <shashinduchamika99@gmail.com>
2026-06-13 14:46:32 +0500

    Create Module

---
+++ __pycache__/create_module/dir/cal.py
@@ -0,0 +1,11 @@
+# addition
+def add(a,b):
+    return a+b
+
+# substraction
+def sub(a,b):
+    return a-b
+
+# multiplication
+def mul(a,b):
+    return a*b
---
+++ __pycache__/create_module/main.py
@@ -0,0 +1,7 @@
+#from dir import cal
+import dir.cal as cal
+
+i =  12
+j = 5
+
+print(cal.add(i,j))
